91383f54d5c70bf6ce0f3f641dcd186de193029b
Yaguang Jia <jiayaguang@foxmail.com>
2023-08-28 15:41:05 +0800

    KYLIN-5795 Remove the check of `hadoop.tmp.dir`

--- src/core-common/src/main/java/org/apache/kylin/common/util/HadoopUtil.java
+++ src/core-common/src/main/java/org/apache/kylin/common/util/HadoopUtil.java
@@ -98,10 +98,6 @@ public class HadoopUtil {
     }
 
     public static Configuration healSickConfig(Configuration conf) {
-        // https://issues.apache.org/jira/browse/KYLIN-953
-        if (StringUtils.isBlank(conf.get("hadoop.tmp.dir"))) {
-            conf.set("hadoop.tmp.dir", "/tmp");
-        }
         //  https://issues.apache.org/jira/browse/KYLIN-3064
         conf.set("yarn.timeline-service.enabled", "false");
 
